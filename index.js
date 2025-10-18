a5cee72cdd68b2b88a6b9e15c45a3f296ad31f2c
PRINCE-DWN <princethakurhero352@gmail.com>
2025-10-18 12:54:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ try {
 }
 
 // ✅ Group Info
-const GROUP_THREAD_ID = "24196335160017473";
-const LOCKED_GROUP_NAME = "🤪 EXIT FUNNY KIDX + TUSHAR BOKA CHUDKE DAFAN 😂";
+const GROUP_THREAD_ID = "775630571985044";
+const LOCKED_GROUP_NAME = "🤪 SANDEEP KI MAA RANDI BAN GAI 😂";
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
